b419cc2ee279f9e23a1b46259c6ed445fd4f4727
BataraKresna <50853838+BataraKresn@users.noreply.github.com>
2025-07-22 06:53:52 +0000

    update

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ if __name__ == '__main__':
     try:
         app.run(
             host='0.0.0.0',
-            port=5000,
+            port=8000,
             debug=False,
             threaded=True
         )
